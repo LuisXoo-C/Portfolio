82655efc7f11fdd8aad6dedd84ca3fa94c5b7015
Luis Angel <lcardonam@unah.hn>
2026-02-08 03:34:01 -0600

    Imagenes de fondo y el proyecto de los puertos

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,11 +1,12 @@
 export const projects = [
+  // Proyecto para Crefisa
   {
-    id: 'credifisa', // ID único
+    id: 'Crefisa', // ID único
     company: 'SEGUROS CREFISA - SEGUROS CREDIFISA',
     title: 'Sistema de Gestión Crediticia',
     shortDescription: 'Transformación digital completa: De procesos manuales a un ecosistema digital automatizado.',
     tags: ['PHP - Laravel', 'Node.js API', 'MySQL', 'On-Premise'],
-    image: 'https://d9zuehkdkxba0.cloudfront.net/wp-content/uploads/2020/08/Seguros-Crefisa-aniversario-27.jpg',
+    image: '/resources/Logo Crefisa.jpg',
     isPrivate: true, // Para mostrar el candado
     stats: {
       value: '400%',
@@ -52,12 +53,13 @@ export const projects = [
       </div>
     `
   },
+  // Proyecto de Sensores y Data Eng
   {
     id: 'iot-system',
     title: 'Sistema IoT + Data Engineering',
-    shortDescription: 'Monitoreo de sensores en tiempo real con ESP32, abarca todo el ciclo de vida de los datos. (Desde la Generacion hasta la Visualizacion)',
-    tags: ['C++', 'Python', 'RedPanda', 'MinIO', 'Postgres DB', 'Metabase'],
-    image: 'https://images.unsplash.com/photo-1518770660439-4636190af475?q=80&w=2070&auto=format&fit=crop',
+    shortDescription: 'Monitoreo de sensores en tiempo real, abarca todo el ciclo de vida de los datos. (Desde la Generación hasta la Visualización de los datos.)',
+    tags: ['Python', 'Docker','C++' , 'FastAPI', 'RedPanda', 'MinIO', 'Postgres DB', 'Metabase'],
+    image: '/resources/Proyecto-sensores.png',
     isPrivate: false,
     stats: {
       value: 'Real-time',
@@ -65,7 +67,61 @@ export const projects = [
       subtext: 'Transmisión de datos sensoriales'
     },
     detailsHTML: `
-      <p class="text-gray-300">Aquí irían los detalles técnicos de tu sistema IoT, cómo usaste los sockets, MQTT, etc...</p>
+      <div class="space-y-6">
+        <div>
+            <h4 class="text-emerald-400 font-bold text-lg mb-2">¿Cómo surgió la idea?</h4>
+            <p class="text-gray-300">.</p>
+        </div>
+        <div>
+            <h4 class="text-emerald-400 font-bold text-lg mb-2">El Desafío</h4>
+            <p class="text-gray-300">Comprender en su mayoría el concepto de lo que hace y por qué un Ingeniero de Datos.</p>
+        </div>
+        <div>
+            <h4 class="text-emerald-400 font-bold text-lg mb-2">Logros</h4>
+            <p class="text-gray-300">• Obtuve un acercamiento más allá de lo macro y me enfrenté a problemas de análisis.</p>
+            <p class="text-gray-300">• Logré hacer y comprender el flujo de un sistema de datos y como está comprendido cada elemento para hacer que los datos raw al concluir tengan sentido para el usuario final.</p>
+            <p class="text-gray-300">• Desacoplamiento del sistema completo, para que cuando se rompa algo no afecte a las demás partes del sistema.</p>
+            <p class="text-gray-300">• Logré hacer un sistema de datos estandarizado y mantenible.</p>
+        </div>
+        <div>
+            <h4 class="text-emerald-400 font-bold text-lg mb-2">Arquitectura</h4>
+            <ul class="list-disc list-inside text-gray-300 space-y-1">
+                <li><strong>:</strong> .</li>
+                <li><strong>:</strong> .</li>
+                <li><strong>:</strong> .</li>
+            </ul>
+        </div>
+      </div>
+    `
+  },
+    // Sistema de los puertos de red.
+  {
+    id: 'Port-Reader',
+    title: 'Sistema para administrar puertos de red',
+    shortDescription: 'Sistema que monitorea el tráfico de puertos especificos de la red, con ello logrando conexión y comunicación con cada dispositivo conectado a dicho puerto.',
+    tags: ['Python', 'Docker', 'Tkinter', 'Postgres DB', 'UDP/TCP'],
+    image: '/resources/Port-Reader.png',
+    isPrivate: false,
+    stats: {
+      value: 'Real-time',
+      label: 'Latencia < 20ms',
+      subtext: 'Comunicación de datos en tiempo real.'
+    },
+    detailsHTML: `
+      <div class="space-y-6">
+        <div>
+            <h4 class="text-emerald-400 font-bold text-lg mb-2">¿Cómo surgió?</h4>
+            <p class="text-gray-300">Necesitaba poder comunicarme con unos microcontroladores mediante TCP y UDP y no encontré un sistema de código abierto que pueda satisfacer mis necesidades, entonces lo hice.</p>
+        </div>
+        <div>
+            <h4 class="text-emerald-400 font-bold text-lg mb-2">Logros</h4>
+            <p class="text-gray-300">• Comunicación TCP Y UDP a dispositivos enlazados.</p>
+            <p class="text-gray-300">• Logré comprender parte de la comunicación de redes.</p>
+            <p class="text-gray-300">• Logré hacer que visualice la comunicación de muchos puertos a la vez.</p>
+            <p class="text-gray-300">• Automatización de comandos según sea el dato que reciba.</p>
+        </div>
+      </div>
     `
   }
-];
+
+  ];
