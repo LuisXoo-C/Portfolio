9c3bb36ace98684cfc05029a71bc65a939ea442b
Luis Angel <lcardonam@unah.hn>
2026-03-31 23:41:51 -0600

    feat: apartado para certificaciones

---
+++ src/data/certifications.js
@@ -0,0 +1,6 @@
+export const certificates = [
+  { id: 1, title: 'AWS Cloud Solutions Architect', issuer: 'Coursera', date: 'Dic 2025', image: '/resources/certifications/miniaturas/Luis_Cardona_AWS-CloudSolutionsArchitec.jpg', url: '/resources/certifications/certificados/Luis_Cardona_AWS-CloudSolutionsArchitect.pdf' },
+  { id: 2, title: 'Fullstack Web Development', issuer: 'Platzi', date: 'Dec 2023', image: '/assets/certs/cert2.jpg', url: '#' },
+  { id: 3, title: 'Cloud Architecture Fundamentals', issuer: 'Udemy', date: 'Jul 2023', image: '/assets/certs/cert3.jpg', url: '#' },
+  { id: 4, title: 'SQL & Data Modeling', issuer: 'LinkedIn Learning', date: 'Jan 2023', image: '/assets/certs/cert4.jpg', url: '#' },
+];
