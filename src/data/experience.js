370d0c592320f4bbe2f9f77ecc7b3fd45fc507db
Luis Angel <lcardonam@unah.hn>
2026-08-20 01:00:00 -0600

    feat: Datos de xp para la pasantia

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,14 +1,31 @@
 export const experiences = [
   {
+    period: 'Mayo 2026 - Presente',
+    position: 'Ingeniero de datos - Práctica Profesional',
+    company: 'Grupo Vesta - Vesta Customs S.A.',
+    description: 'Estoy realizando tareas de ingeniería de datos e ingeniería de software, automatizaciones de procesos, incluyendo ETL, análisis de datos, visualización con Power BI y desarrollo de sistemas para consumo interno.',
+    achievements: [
+      'API Validador de CAI: Hecha con automatización web scraping sustituyendo procesos manuales por consumo directo de APIs, con generación de plantilla de comprobación en PDF y JSON o solo JSON, reduciendo los tiempos operativos en la validación de CAI. Siendo necesaria para más de 3 departamentos.',
+      'Estandarización de nomenclaturas para mejorar la consistencia y calidad los BI. Facilitando la captura de errores de actualización o reportes desactualizados desde API service de Power BI.',
+      'API Jetstereo: Sabana de datos estructurados para el control de tiempos aduaneros y administración de contenedores.',
+      'API Agrolibano: ',
+      'Reportes en Power BI para la facilitar la captura de visitas a clientes y licitaciones próximas a vencer.',
+      'Sistema de consumo interno para cumplir con los tiempos de entrega de Record Keeping de Cargill. Hecho bajo los estándares de SDLC.'
+    ],
+    technologies: ['Python', 'ETL', 'Power BI', 'Web Scraping', 'Angular 21', 'C#', 'SQL SERVER', "API's", 'SDLC', 'Git'],
+    references: [
+    ],
+  },
+  {
     period: 'Enero 2026 - Junio 2026',
-    position: 'Pasante de Desarrollo de Sistemas',
+    position: 'Desarrollador de Sistemas - Pasante',
     company: 'Universidad Nacional Autónoma de Honduras (UNAH) - DIPP',
     description: 'Asumí la responsabilidad en la modernización del Sistema de Gestión Acádemica de la Universidad Nacional Autónoma de Honduras. Para de más de 70,000 usuarios.',
     achievements: [
       'Implementación de Clean Architecture utilizando Angular y TypeScript para garantizar un código modular, escalable y de fácil mantenimiento.',
       'Aplicación de metodologías del Ciclo de Vida del Desarrollo de Software (SDLC) y estándares de ingeniería para rediseñar y escalar el sistema.'
     ],
-    technologies: ['Angular', 'TypeScript', 'JS', 'CSS','HTML5', 'Clean Architecture', 'SDLC', ],
+    technologies: ['Angular', 'TypeScript', 'JS', 'CSS','HTML5', 'Clean Architecture', 'SDLC'],
     references: [
     ],
 
@@ -26,7 +43,7 @@ export const experiences = [
       'Diseñé arquitecturas de datos robustas que permitieron la expansión y el crecimiento del parque de dispositivos IoT activos de la organización',
       'Logré un incrementando en la velocidad de entrega y fiabilidad de los proyectos.'
     ],
-    technologies: ['C++', 'ESP32', 'Teltonika IoT', 'Git', 'Clean Architecture', 'SDLC', 'DBA - SQL'],
+    technologies: ['C++', 'ESP32', 'Teltonika IoT', 'Git', 'Clean Architecture', 'SDLC', 'DBA - SQL', 'T-SQL'],
     references: [
     ],
 
